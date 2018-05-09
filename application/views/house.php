cb9e6f66d9458059e944e08b790393de41d4aefa
ardhapanduwinata <ardhapanduwinata@gmail.com>
2018-05-09 10:10:07 +0700

    benerin view house atau buat user

--- application/views/house.php
+++ application/views/house.php
@@ -9,14 +9,15 @@
     <title>One Page Wonder - Start Bootstrap Template</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+    <link href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i"
+          rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="<?= base_url('assets/house/css/one-page-wonder.min.css')?>" rel="stylesheet">
+    <link href="<?= base_url('assets/house/css/one-page-wonder.min.css') ?>" rel="stylesheet">
 
 </head>
 
@@ -26,7 +27,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
         <a class="navbar-brand" href="#">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
@@ -35,7 +37,7 @@
                     <a class="nav-link" href="#">Sign Up</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?= base_url()?>index.php/house/login">Log In</a>
+                    <a class="nav-link" href="<?= base_url() ?>index.php/house/login">Log In</a>
                 </li>
             </ul>
         </div>
@@ -55,51 +57,54 @@
     <div class="bg-circle-3 bg-circle"></div>
     <div class="bg-circle-4 bg-circle"></div>
 </header>
-<?php foreach($array_buku as $key) { ?>
-<section>
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-6 order-lg-2">
-                <div class="p-5">
-                    <img style="width: 250;" src="<?= base_url('assets/img/').$key['image']?>" alt="">
+<?php foreach ($array_buku as $key) {
+    if (($key['id_buku'])%2 == 0) { ?>
+    <section>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 order-lg-2">
+                    <div class="p-5">
+                        <img style="width: 250px;" src="<?= base_url('assets/img/') . $key['image'] ?>" alt="">
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-6 order-lg-1">
-                <div class="p-5">
-                    <h2 class="display-4"><?= $key['nama_buku']?></h2>
-                    <p><?= $key['sinopsis']?></p>
-                    <button class="btn btn-outline-primary" type="button">View More</button>
+                <div class="col-lg-6 order-lg-1">
+                    <div class="p-5">
+                        <h2 class="display-4"><?= $key['nama_buku'] ?></h2>
+                        <p><?= $key['sinopsis'] ?></p>
+                        <button class="btn btn-outline-primary" type="button">View More</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<?php } ?>
-<!---->
-<!--<section>-->
-<!--    <div class="container">-->
-<!--        <div class="row align-items-center">-->
-<!--            <div class="col-lg-6">-->
-<!--                <div class="p-5">-->
-<!--                    <img class="img-fluid rounded-circle" src="--><?//= base_url('assets/house/img/02.jpg')?><!--" alt="">-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="col-lg-6">-->
-<!--                <div class="p-5">-->
-<!--                    <h2 class="display-4">We salute you!</h2>-->
-<!--                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</section>-->
-<!---->
+    </section>
+        <?php } else { ?>
+    <section>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="p-5">
+                        <img style="width: 250px;" src="<?= base_url('assets/img/') . $key['image'] ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="p-5">
+                        <h2 class="display-4"><?= $key['nama_buku'] ?></h2>
+                        <p><?= $key['sinopsis'] ?></p>
+                        <button class="btn btn-outline-primary" type="button">View More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php }
+     } ?>
 <!--<section>-->
 <!--    <div class="container">-->
 <!--        <div class="row align-items-center">-->
 <!--            <div class="col-lg-6 order-lg-2">-->
 <!--                <div class="p-5">-->
-<!--                    <img class="img-fluid rounded-circle" src="--><?//= base_url('assets/house/img/03.jpg')?><!--" alt="">-->
+<!--                    <img class="img-fluid rounded-circle" src="-->
+<? //= base_url('assets/house/img/03.jpg')?><!--" alt="">-->
 <!--                </div>-->
 <!--            </div>-->
 <!--            <div class="col-lg-6 order-lg-1">-->
@@ -121,8 +126,8 @@
 </footer>
 
 <!-- Bootstrap core JavaScript -->
-<script src="<?= base_url('assets/vendor/jquery/jquery.min.js')?>"></script>
-<script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+<script src="<?= base_url('assets/vendor/jquery/jquery.min.js') ?>"></script>
+<script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 
 </body>
 
